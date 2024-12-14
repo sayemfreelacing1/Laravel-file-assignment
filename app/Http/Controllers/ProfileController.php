4a48e1197f922c202c5f78ba2487f199ff745b3f
Sayem Rahman <sayemfreelancing1@gmail.com>
2024-12-14 12:24:45 +0600

    first commit

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProfileController extends Controller
+{
+    public function index($id)
+    {
+        $name = "Donald Trump";
+        $age = "75";
+
+        $data = [
+            'id' => $id,
+            'name' => $name,
+            'age' => $age
+        ];
+
+        // Set cookie parameters
+        $cookieName = 'access_token';
+        $cookieValue = '123-XYZ';
+        $cookieMinutes = 1;
+        $cookiePath = '/';
+        $cookieDomain = $_SERVER['SERVER_NAME'];
+        $cookieSecure = false;
+        $cookieHttpOnly = true;
+
+        $cookie = cookie($cookieName, $cookieValue, $cookieMinutes, $cookiePath, $cookieDomain, $cookieSecure, $cookieHttpOnly);
+        
+        return response()->json($data, 200)->cookie($cookie);
+    }
+}
